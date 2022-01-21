5a7a995b50f0257118b0a08c7641938ae2c92e7c
Anish <anish.arvind407@gmail.com>
2022-01-21 21:42:25 +0100

    Added CallerArgumentExpression

---
+++ YoutubeSamples/OtherExtensions/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace YoutubeSamples.OtherExtensions
+{
+    public static class ObjectExtensions
+    {
+        public static void ThrowIfNull<T>(this T  value, [CallerArgumentExpression("value")] string valueName= null)
+        {
+            Console.WriteLine($"{valueName} is the Name of the Argument");
+           // if (value == null) throw new ArgumentNullException($"{valueName} is null");
+        }
+
+
+    }
+}
---
+++ YoutubeSamples/OtherExtensions/OtherExtensionsProgram.cs
@@ -0,0 +1,18 @@
+﻿using static YoutubeSamples.OtherExtensions.ObjectExtensions;
+MyClass mine1 = new MyClass();
+MyClass mine2 = new MyClass();
+MyClass mine3 = new MyClass();
+
+mine1.ThrowIfNull();   // mine1 is the Name of the Argument
+mine2.ThrowIfNull();  // mine2 is the Name of the Argument
+mine3.ThrowIfNull(); // mine3 is the Name of the Argument
+
+public class MyClass
+{
+    public string  Name { get; set; }
+}
+
+
+
+
+
